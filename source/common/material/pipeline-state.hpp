aa76d89188a5e1329b5f1e3ce7b2e95fcbfc7090
John Maurice <johnmaurice71@gmail.com>
2022-11-19 19:04:51 +0200

    Req 4 done

--- source/common/material/pipeline-state.hpp
+++ source/common/material/pipeline-state.hpp
@@ -41,7 +41,21 @@ namespace our {
         // This function should set the OpenGL options to the values specified by this structure
         // For example, if faceCulling.enabled is true, you should call glEnable(GL_CULL_FACE), otherwise, you should call glDisable(GL_CULL_FACE)
         void setup() const {
-            //TODO: (Req 4) Write this function
+            //DONE: (Req 4) Write this function
+            glColorMask(colorMask.r, colorMask.g, colorMask.b, colorMask.a);
+            glDepthMask(depthMask);
+
+            faceCulling.enabled ? glEnable(GL_CULL_FACE) : glDisable(GL_CULL_FACE); 
+            glCullFace(faceCulling.culledFace);
+            glFrontFace(faceCulling.frontFace);
+
+            depthTesting.enabled ? glEnable(GL_DEPTH_TEST) : glDisable(GL_DEPTH_TEST); 
+            glDepthFunc(depthTesting.function);
+
+            blending.enabled ? glEnable(GL_BLEND) : glDisable(GL_BLEND);
+            glBlendEquation(blending.equation);
+            glBlendColor(blending.constantColor.r, blending.constantColor.g, blending.constantColor.b, blending.constantColor.a);
+            glBlendFunc(blending.sourceFactor, blending.destinationFactor); 
         }
 
         // Given a json object, this function deserializes a PipelineState structure
