6dd784189089f48b78c5ed7a4913146b57c4a2c2
John Maurice <johnmaurice71@gmail.com>
2022-11-19 19:46:44 +0200

    Req 5 done

--- source/common/texture/texture-utils.cpp
+++ source/common/texture/texture-utils.cpp
@@ -35,6 +35,11 @@ our::Texture2D* our::texture_utils::loadImage(const std::string& filename, bool 
     our::Texture2D* texture = new our::Texture2D();
     //Bind the texture such that we upload the image data to its storage
     //TODO: (Req 5) Finish this function to fill the texture with the data found in "pixels"
+    //glActiveTexture(GL_TEXTURE0); //left todo as i am unsure if this is the function responsible for selecting a texture unit
+    texture->bind();
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, size.x, size.y, 0, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
+    if (generate_mipmap) glGenerateMipmap(GL_TEXTURE_2D);
+    texture->unbind();
     
     stbi_image_free(pixels); //Free image data after uploading to GPU
     return texture;
